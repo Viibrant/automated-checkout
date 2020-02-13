b392a04c44787eac4c903972f3611dbe9ee5bf53
connor <connorbrook@googlemail.com>
2020-02-13 10:26:39 +0000

    webcam display code working

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Execution successful.")
+import numpy as np
+import cv2
+from PIL import Image
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    ret, frame = cap.read()
+    img = Image.fromarray(frame, 'RGB')
+    print(img)
+    if ret == True:
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('l'):
+            break
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
