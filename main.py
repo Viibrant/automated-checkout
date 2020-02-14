d8e0045e51f47d92d6cbd41d7897d846237ba85b
connor <connorbrook@googlemail.com>
2020-02-14 10:37:51 +0000

    FUCKING WORKING OHLY SHIT FUCK YES, ran into lots of issues with shape of the numpy arra ythough

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 import numpy as np
 import cv2
+import keras
+from keras.preprocessing.image import img_to_array
+from keras.applications import mobilenet
+from keras.preprocessing.image import load_img
+from keras.applications.imagenet_utils import decode_predictions
 from PIL import Image
-
+from numpy import asarray
 cap = cv2.VideoCapture(0)
+model = mobilenet.MobileNet(weights='imagenet')
 
 while(True):
     ret, frame = cap.read()
+    # get the image and convert it into a numpy array and into a format for our model
     img = Image.fromarray(frame, 'RGB')
-    print(img)
+    img = img.resize((224,224), Image.ANTIALIAS)
+    np_image = img_to_array(img)
+    image_batch = np.expand_dims(np_image, axis=0)
+    processed_image = mobilenet.preprocess_input(image_batch.copy())    
+
+
+    # actual machine learning part
+    predictions = model.predict(processed_image)
+    label = decode_predictions(predictions)
+    print(label)
+    
     if ret == True:
         cv2.imshow('frame',frame)
         if cv2.waitKey(1) & 0xFF == ord('l'):
-            break
+            break        
     else:
         break
 
