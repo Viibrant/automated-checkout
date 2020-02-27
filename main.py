cf6d30673c2a369452eb439622ab176f8b485450
connor <connorbrook@googlemail.com>
2020-02-27 13:02:07 +0000

    added some nicer formatting on the output

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ from keras.preprocessing.image import load_img
 from keras.applications.imagenet_utils import decode_predictions
 from PIL import Image
 from numpy import asarray
+import process
 cap = cv2.VideoCapture(0)
 model = mobilenet.MobileNet(weights='imagenet')
+
+
 
 while(True):
     ret, frame = cap.read()
@@ -17,14 +20,14 @@ while(True):
     img = img.resize((224,224), Image.ANTIALIAS)
     np_image = img_to_array(img)
     image_batch = np.expand_dims(np_image, axis=0)
-    processed_image = mobilenet.preprocess_input(image_batch.copy())    
-
+    processed_image = mobilenet.preprocess_input(image_batch.copy())
 
     # actual machine learning part
     predictions = model.predict(processed_image)
-    label = decode_predictions(predictions)
-    print(label)
-    
+    labels = decode_predictions(predictions)
+    for label in labels:
+        print("Prediction: %s, Probability: %s"%(label[0][1], label[0][2]))
+ 
     if ret == True:
         cv2.imshow('frame',frame)
         if cv2.waitKey(1) & 0xFF == ord('l'):
