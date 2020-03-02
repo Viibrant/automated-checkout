10cf1695db89a855ecf9d5d3eedf98b3e34bd0fb
Connor <connorbrook@googlemail.com>
2020-03-02 17:49:38 +0000

    gpu support

--- current-working.py
+++ current-working.py
@@ -8,10 +8,25 @@ from keras.applications.imagenet_utils import decode_predictions
 from PIL import Image
 from numpy import asarray
 import process
+import tensorflow as tf
+
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
+
+
+
 cap = cv2.VideoCapture(0)
 model = mobilenet.MobileNet(weights='imagenet')
-
-
 
 while(True):
     ret, frame = cap.read()
