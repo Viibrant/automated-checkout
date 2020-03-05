eeaab8022954fe04de644d62dc767bea9086261e
connor <connorbrook@googlemail.com>
2020-03-05 17:14:59 +0000

    created new route, /predict, which, i know is bad but fuck you whatever, uses the global variable frame consisting of the byte data of the webcam currently in use and then forms a prediction using an iterable by yielding each prediction which is then returned as a Response with mimetype 'text/plain', doesn't update web page yet though which is an issue

--- web.py
+++ web.py
@@ -1,18 +1,18 @@
 from flask import Flask, render_template, Response
 from camera import VideoCamera
-from prediction import NeuralNetwork
+from neuralnetwork import NeuralNetwork
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 def gen(camera):
-    model = NeuralNetwork()
+    global frame
     while True:
         frame = camera.get_frame()
-        print(model.predict(frame))
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
@@ -21,5 +21,20 @@ def video_feed():
     return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+@app.route('/predict')
+def predict():
+    model = NeuralNetwork()
+    def pred(model):
+        global frame
+        while True:
+            predictions = model.predict(frame)
+            for prediction in predictions:
+                yield ("Prediction: %s, Probability: %s\n" 
+                    % (prediction[0][1], prediction[0][2]))
+    print(True)
+    return Response(pred(model),mimetype='text/plain')
+        
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, threaded=False)
+    app.debug = True
+    app.run(host='0.0.0.0', threaded=True)
